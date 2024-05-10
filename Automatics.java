42f64f46363fcccb2a76cba9d81f4444a21c579f
KevDevEv <kdong0253@bths.edu>
2024-05-10 18:33:37 -0400

    Add files via upload

---
+++ Automatics.java
@@ -0,0 +1,33 @@
+import java.util.Map;
+
+public class Automatics extends Inventory
+{
+    int maxCapacity;
+    
+    Automatics(int maxCapacity) {this.maxCapacity = maxCapacity;}
+    Automatics() {this.maxCapacity = 0;}
+
+    public void setMaxCapacity(int maxCapacity) {this.maxCapacity = maxCapacity;}
+    public int getMaxCapacity() {return maxCapacity;}
+
+    public Boolean inCapacity() {
+        int totalStock = 0;
+        for (Map.Entry<String,Data> entry : inventory.entrySet()) totalStock += entry.getValue().getStock();
+        if (totalStock > maxCapacity) return false;
+        return true;
+    }
+
+    public void levelOff() {
+        for (Map.Entry<String,Data> entry : inventory.entrySet()) entry.getValue().level();
+    }
+    
+    public void simulate(int time) {
+        for (int i = 0; i < time; i++) {
+            for (Map.Entry<String,Data> entry : inventory.entrySet()) {
+                if (!inCapacity()) levelOff();
+                else if (!entry.getValue().inBounds()) entry.getValue().level();
+                entry.getValue().tick();
+            }
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Inventory inv = new Inventory();
+        /*Data data = new Data("a1", 100, 0, 0, 10, 10, 100, 50, 200);
+        inv.addItem("ab", data);
+        data = new Data("a2", 50, 0, 0, 10, 10, 50, 100, 0);
+        inv.addItem("ba", data);*/
+        inv.log("test.txt");
+        /*inv.print();
+        System.out.println(System.getProperty("user.dir"));*/
+    }
+}
